b850568a06453a41f9b2aa7b2405050098c16f8a
Marcelo Soares Moura <105823851+MarceloSM73@users.noreply.github.com>
2024-09-09 11:36:41 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista profissional brasileira que se tornou a mais jovem medalhista olímpica da história do Brasil. Com seu estilo único e carisma, ela conquistou fãs ao redor do mundo.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca por suas técnicas de projeção e imobilização. Ela já conquistou diversas medalhas em competições nacionais e internacionais.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que se destacou nos Jogos Olímpicos de Tóquio. Conhecida por sua força e elegância, ela conquistou diversas medalhas em diferentes modalidades da ginástica artística.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    }
+];
